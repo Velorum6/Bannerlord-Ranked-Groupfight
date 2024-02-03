3c1ed512ddde4aab2f3ea973489eeac3a1ea2ce1
krex <namakan.server@gmail.com>
2024-02-03 18:34:31 +0100

    RemoveHorsesCommand: Fix "Collection was modified during Enumeration" error, formatting

--- BLAdminTools/ChatCommands/AdminCommands/RemoveHorsesCommand.cs
+++ BLAdminTools/ChatCommands/AdminCommands/RemoveHorsesCommand.cs
@@ -1,4 +1,5 @@
-﻿using DoFAdminTools.Helpers;
+﻿using System.Linq;
+using DoFAdminTools.Helpers;
 using TaleWorlds.MountAndBlade;
 
 namespace DoFAdminTools.ChatCommands.AdminCommands
@@ -7,24 +8,29 @@ namespace DoFAdminTools.ChatCommands.AdminCommands
     {
         public override string CommandText => "removeHorses";
 
-        public override string Description =>
-            "Removes all unmounted horses.";
+        public override string Description => "Removes all unmounted horses.";
 
         public override bool Execute(NetworkCommunicator executor, string args)
         {
-
-            if (Mission.Current.MountsWithoutRiders.Count > 0)
+            if (Mission.Current?.MountsWithoutRiders.Count == 0)
             {
-                foreach (var pair in Mission.Current.MountsWithoutRiders)
-                {
-                    pair.Key?.FadeOut(true, true);
-                }
-                Helper.SendMessageToAllPeers($"{executor.UserName} removed all stray horses.");
-                return true;
+                Helper.SendMessageToPeer(executor, "No unmounted horses were found.");
+                return false;
             }
 
-            Helper.SendMessageToPeer(executor, "No unmounted horses were found.");
-            return false;
+            // collect all first as looping over keyset & removing directly would cause change in Collection during looping
+            var agents = Mission.Current.MountsWithoutRiders
+                .Where(pair => pair.Key != null)
+                .Select(pair => pair.Key)
+                .ToList();
+            
+            foreach (var agent in agents)
+            {
+                agent.FadeOut(true, true);
+            }
+            
+            Helper.SendMessageToAllPeers($"{executor.UserName} removed all stray horses.");
+            return true;
         }
     }
 }
